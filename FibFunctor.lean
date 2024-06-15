12cb9b0d217596d8117669f6e2e4860a4ef530e9
Aren Guralp <142130640+guralpa@users.noreply.github.com>
2024-06-15 11:52:58 -0400

    Define Category Nat and fib_functor, start proving fib_functor has a left adjoint

--- FibFunctor.lean
+++ FibFunctor.lean
@@ -1,3 +1,61 @@
 -- This module serves as the root of the `FibFunctor` library.
 -- Import modules here that should be built as part of the library.
 import «FibFunctor».Basic
+import Mathlib.CategoryTheory.Category.Basic
+import Mathlib.CategoryTheory.Functor.Basic
+import Mathlib.CategoryTheory.Adjunction.Basic
+import Mathlib.CategoryTheory.Limits.HasLimits
+import Mathlib.CategoryTheory.Adjunction.AdjointFunctorTheorems
+import Mathlib.Algebra.Divisibility.Basic
+import Mathlib.Algebra.Group.Nat
+import Mathlib.Data.Nat.Fib.Basic
+
+universe u
+
+namespace CategoryTheory
+
+instance : Category.{u} Nat where
+  Hom A B := ULift (PLift (A ∣ B))
+  id A := ⟨⟨dvd_refl A⟩⟩
+  comp X Y := ⟨⟨dvd_trans X.down.down Y.down.down⟩⟩
+
+def fib_functor : Nat ⥤ Nat where
+  obj := Nat.fib
+  map := by
+    intro X Y h
+    exact ⟨⟨Nat.fib_dvd X Y h.down.down⟩⟩
+    -- TODO: prove fib_dvd yourself
+  map_id := by
+    intro a
+    dsimp
+    apply congrArg
+    apply congrArg
+    rfl
+  map_comp := by
+    intro a b c h h'
+    dsimp
+    apply congrArg
+    apply congrArg
+    rfl
+
+instance : Limits.HasLimitsOfSize Nat where
+  has_limits_of_shape := by
+    sorry
+
+instance : Limits.PreservesLimitsOfSize fib_functor where
+  preservesLimitsOfShape := by
+    sorry
+
+lemma nat_has_limits : Limits.HasLimits Nat := by
+  sorry
+
+lemma fib_solset : SolutionSetCondition.{0} fib_functor := by
+  rw [SolutionSetCondition]
+  intro a
+  use Nat
+
+
+set_option pp.all true
+
+theorem fib_has_left_adjoint : Functor.IsRightAdjoint.{0, 0} fib_functor :=
+  isRightAdjoint_of_preservesLimits_of_solutionSetCondition fib_functor fib_solset
